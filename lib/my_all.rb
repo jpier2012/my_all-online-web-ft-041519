487a945923167a1c6779c4069975791a506902e3
James Pier <jpier2012@gmail.com>
2019-04-15 13:14:27 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_all?(collection)
   new_array = []
   collection.each {|element|
-    if yield[element] == true
+    if yield(element) == true
       new_array << element
     end
   }
